35a7f5bcd838a1629c408e184cf75ae4ffed5983
Alejandro Alba <25alejalb1@cms.edu.do>
2024-03-13 15:45:58 -0400

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// the WPILib BSD license file in the root directory of this project.[]
 
 package frc.robot;
 
@@ -98,7 +98,6 @@ public class RobotContainer
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 
     driverPS4.cross().onTrue((Commands.runOnce(drivebase::zeroGyro)));
-    driverPS4.square().onTrue(Commands.runOnce(drivebase::addFakeVisionReading));
 
     // driverPS4.x().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
   }
